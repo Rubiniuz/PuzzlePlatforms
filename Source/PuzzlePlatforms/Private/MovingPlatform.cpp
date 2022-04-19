f9fe5c7fb0fc7e8214999ca9e856fc5e5ffb0017
“username” <emailaddr>
2022-04-19 19:04:37 +0200

    Add Moving Platform With array of points

--- Source/PuzzlePlatforms/Private/MovingPlatform.cpp
+++ Source/PuzzlePlatforms/Private/MovingPlatform.cpp
@@ -21,6 +21,11 @@ void AMovingPlatform::BeginPlay()
 		SetReplicates(true); // allow replication
 		SetReplicateMovement(true); // allow movement to be replicated
 	}
+	
+	Original = GetTransform();
+	if(TargetLocations.Num() > 0)
+		GlobalStartLocation = GetTransform().TransformPosition(TargetLocations[LocationIndex]);
+		GlobalTarget = GetTransform().TransformPosition(TargetLocations[LocationIndex + LocationIncrement]);
 }
 
 void AMovingPlatform::Tick(float DeltaSeconds)
@@ -30,7 +35,45 @@ void AMovingPlatform::Tick(float DeltaSeconds)
 	if(HasAuthority()) // is this the server?
 	{
 		FVector Location = GetActorLocation();
-		Location += FVector(MoveSpeed * DeltaSeconds,0,0);
+
+		float JourneyLength = (GlobalTarget - GlobalStartLocation).Length();
+		float JourneyTravelled = (Location - GlobalStartLocation).Length();
+		
+		if(JourneyTravelled > JourneyLength)
+		{
+			LocationIndex += LocationIncrement;
+
+			if(LocationIndex == TargetLocations.Num() - 1)
+			{
+				if(bShouldLoop == true)
+				{
+					LocationIndex = -1;
+					LocationIncrement = 1;
+					GlobalStartLocation = Original.TransformPosition(TargetLocations[TargetLocations.Num() - 1]);
+					GlobalTarget = Original.TransformPosition(TargetLocations[0]);
+				}
+				else
+				{
+					LocationIncrement = -1;
+					GlobalStartLocation = Original.TransformPosition(TargetLocations.Last());
+					GlobalTarget = Original.TransformPosition(TargetLocations[TargetLocations.Num() - 2]);
+				}
+			}
+			else if(LocationIndex == 0)
+			{
+				LocationIncrement = 1;
+				GlobalStartLocation = Original.TransformPosition(TargetLocations[0]);
+				GlobalTarget = Original.TransformPosition(TargetLocations[1]);
+			}
+			else
+			{
+				GlobalStartLocation = Original.TransformPosition(TargetLocations[LocationIndex]);
+				GlobalTarget = Original.TransformPosition(TargetLocations[LocationIndex + LocationIncrement]);
+			}
+		}
+		
+		const FVector Direction = (GlobalTarget - GlobalStartLocation).GetSafeNormal();
+		Location += MoveSpeed * DeltaSeconds * Direction;
 		SetActorLocation(Location);
 	}
 }
--- Source/PuzzlePlatforms/Public/MovingPlatform.h
+++ Source/PuzzlePlatforms/Public/MovingPlatform.h
@@ -17,5 +17,23 @@ public:
 	virtual void Tick(float DeltaSeconds) override;
 
 	UPROPERTY(EditAnywhere)
+	bool bShouldReturn = true;
+
+	UPROPERTY(EditAnywhere)
+	bool bShouldLoop = false;
+	
+	UPROPERTY(EditAnywhere)
 	float MoveSpeed = 20.0f;
+
+	UPROPERTY(EditAnywhere, Meta = (MakeEditWidget = true)) // Metadata adds the widgetGizmo
+	TArray<FVector> TargetLocations = TArray<FVector>();
+
+	UPROPERTY(EditAnywhere)
+	int LocationIndex = 0;
+
+private:
+	FVector GlobalTarget;
+	FVector GlobalStartLocation;
+	FTransform Original;
+	int LocationIncrement = 1;
 };
